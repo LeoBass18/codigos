00311503a58be3e05724c0e1b30942da43ecd468
Leonardo <128001644+LeoBass18@users.noreply.github.com>
2024-03-24 11:10:32 -0300

    Add files via upload

---
+++ trabalho_final.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import os
+import pyodbc
+
+pasta_dados = r"C:\Users\leonardo.mello\OneDrive - Programmers Beyond IT\Área de Trabalho\Léo\UNIFOR\PYTHON\dados_ibge-20240323T140953Z-001\dados_ibge"
+
+# Função para ler um arquivo e retornar como uma lista de linhas
+def ler_arquivo(caminho_arquivo):
+    with open(caminho_arquivo, 'r') as arquivo:
+        linhas = arquivo.readlines()[1:]  # Ignora a primeira linha (cabeçalho)
+    return linhas
+
+# Função para juntar todos os arquivos em um único conjunto de dados
+def juntar_dados(pasta_dados):
+    dados_completos = []
+    for arquivo in os.listdir(pasta_dados):
+        caminho_arquivo = os.path.join(pasta_dados, arquivo)
+        dados_arquivo = ler_arquivo(caminho_arquivo)
+        dados_completos.extend(dados_arquivo)
+    return dados_completos
+
+# Executar a função para juntar todos os dados
+dados_juntos = juntar_dados(pasta_dados)
+
+
+
+
+import pyodbc
+
+# Parâmetros de conexão com o SQL Server
+server = 'NTB-G0ZQMS3\SQLEXPRESS' 
+database = 'desafio_final'  
+username = 'PROGRAMMERS\\leonardo.mello' 
+# Não forneci a senha, pois estamos usando autenticação do Windows
+
+# String de conexão
+conn_str = f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};Trusted_Connection=yes'
+
+# Estabelecendo a conexão com o banco de dados
+conn = pyodbc.connect(conn_str)
+cursor = conn.cursor()
+
+# Inserindo os dados na tabela
+for linha in dados_juntos:
+    valores = linha.strip().split(';')  # Dividir cada linha em valores individuais
+    cursor.execute("INSERT INTO IBGE_2 (COD_UF, COD_MUN, COD_ESPECIE, LATITUDE, LONGITUDE, NV_GEO_COORD) VALUES (?, ?, ?, ?, ?, ?)", valores)
+
+# Confirmar a transação e fechar a conexão
+conn.commit()
+conn.close()
+
+print("Dados inseridos com sucesso na tabela.")
+
+
+
+# Confirmar a transação e fechar a conexão
+conn.commit()
+conn.close()
+
+print("Dados inseridos com sucesso no SQL Server.")
+
+
+
+
+
+print(dados_juntos[:1])  # Imprime as primeiras 10 linhas dos dados
+
+
+
+
+
+
